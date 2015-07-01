f84ce23688c30e0cf913c0464e55d5739c7cae3d
U-266-B4\schueler <dominik.suess@outlook.at>
2015-07-01 09:55:35 +0200

    Finished NN implementation - UNTESTED

--- source/dneat/neuralnetwork/package.d
+++ source/dneat/neuralnetwork/package.d
@@ -141,7 +141,341 @@ public:
 
 	}
 
-	// Accessor functions
+	void activateFast()
+	{
+		// Loop connections
+		for(uint i = 0; i < connections.length;i++)
+		{
+			connections[i].signal = neurons[connections[i].sourceNeuronIdx].activation
+				* connections[i].weight;
+		}
+		// Add signals to the target neurons
+		// Memory heavy
+		for(uint i = 0; i < connections.length;i++)
+		{
+			neurons[connections[i].targetNeuronIdx].activesum += connections[i].signal;
+		}
+
+		// Loop activesums, pass signals through the activation function
+		// store result back to activations
+		// skip inputs since they don't get activated
+
+		for(uint i = numInputs; i < neurons.length) i++)
+		{
+			double x = neurons[i].activesum;
+			neurons[i].activesum = 0;
+
+			double y = 0.0;
+			y = af_sigmoidUnsigned(x,neurons[i].a,neurons[i].b);
+			neurons[i].activesum = y;
+		}
+	}
+
+	void activate()
+	{
+		// Loop connections
+		for(uint i = 0; i < connections.length;i++)
+		{
+			connections[i].signal = neurons[connections[i].sourceNeuronIdx].activation
+				* connections[i].weight;
+		}
+		// Add signals to the target neurons
+		// Memory heavy
+		for(uint i = 0; i < connections.length;i++)
+		{
+			neurons[connections[i].targetNeuronIdx].activesum += connections[i].signal;
+		}
+		// Loop activesums, pass signals through the activation function
+		// store result back to activations
+		// skip inputs since they don't get activated
+		foreach(ref Neuron n;parallel(neurons[numInputs..$]))
+		{
+			double x = n.activesum;
+			n.activesum = 0;
+
+			double y = 0.0;
+
+			switch(n.activationFunctionType)
+			{
+				case signedSigmoid:
+					y = af_sigmoidSigned(x,n.a,n.b);
+					break;
+				default:
+					y = af_sigmoidUnsigned(x,n.a,n.b);
+					break;
+			}
+			n.activation = y;
+		}
+	}
+	void activateUseInternalBias()
+	{
+		// Loop connections
+		for(uint i = 0; i < connections.length;i++)
+		{
+			connections[i].signal = neurons[connections[i].sourceNeuronIdx].activation
+				* connections[i].weight;
+		}
+		// Add signals to the target neurons
+		// Memory heavy
+		for(uint i = 0; i < connections.length;i++)
+		{
+			neurons[connections[i].targetNeuronIdx].activesum += connections[i].signal;
+		}
+		// Loop activesums, pass signals through the activation function
+		// store result back to activations
+		// skip inputs since they don't get activated
+		foreach(ref Neuron n;parallel(neurons[numInputs..$]))
+		{
+			double x = n.activesum + n.bias;
+			n.activesum = 0;
+
+			double y = 0.0;
+
+			switch(n.activationFunctionType)
+			{
+				case signedSigmoid:
+					y = af_sigmoidSigned(x,n.a,n.b);
+					break;
+				default:
+					y = af_sigmoidUnsigned(x,n.a,n.b);
+					break;
+			}
+			n.activation = y;
+		}
+	}
+	void activateLeaky(double time)
+	{
+		// Loop connections
+		for(uint i = 0; i < connections.length;i++)
+		{
+			connections[i].signal = neurons[connections[i].sourceNeuronIdx].activation
+				* connections[i].weight;
+		}
+		// Add signals to the target neurons
+		// Memory heavy
+		for(uint i = 0; i < connections.length;i++)
+		{
+			neurons[connections[i].targetNeuronIdx].activesum += connections[i].signal;
+		}
+
+		foreach(ref Neuron n;parallel(neurons[numInputs..$]))
+		{
+			tconst = time / n.timeconst;
+			n.membranePotential = (1.0 -tconst)
+				* n.membranePotential
+				+ tconst
+				* n.activesum;
+
+		}
+		// Loop activesums, pass signals through the activation function
+		// store result back to activations
+		// skip inputs since they don't get activated
+		foreach(ref Neuron n;parallel(neurons[numInputs..$]))
+		{
+			double x = n.membranePotential + n.bias;
+			n.activesum = 0;
+
+			double y = 0.0;
+
+			switch(n.activationFunctionType)
+			{
+				case signedSigmoid:
+					y = af_sigmoidSigned(x,n.a,n.b);
+					break;
+				default:
+					y = af_sigmoidUnsigned(x,n.a,n.b);
+					break;
+			}
+			n.activation = y;
+		}
+	}
+
+	void flush()
+	{
+		foreach(ref Neuron n;parallel(neurons))
+		{
+			n.activation = 0;
+			n.activesum = 0;
+			n.membranePotential = 0;
+		}
+	}
+
+	void input(double[] inputs)
+	{
+		assert(inputs.length == numInputs,"Input array must be the same size as the number of Inputs in the NN");
+
+		for(uint i = 0; i < numInputs;i++)
+		{
+			neurons[i].activation = inputs[i];
+		}
+	}
+
+	double[] output()
+	{
+		double[] outputval;
+		for(uint i = 0; i < numOutputs; i++)
+		{
+			outputval ~= neurons[i + numInputs].activation;
+		}
+		return outputval;
+	}
+
+	void adapt(Parameters params)
+	{
+		double maxWeight = double.min;
+		// find absolute magnitude of the weight
+		for(uint i = 0; i < connections.length; i++)
+		{
+			if(abs(connections[i].weight) > maxWeight)
+			{
+				maxWeight = connections[i].weight;
+			}
+		}
+
+		for(uint i = 0; connections.length; i++)
+		{
+			//////////////////////////////////////
+			// modify weight of that connection //
+			//////////////////////////////////////
+			double incomingNeuronActivation = neurons[connections[i].sourceNeuronIdx].activation;
+			double outgoingNeuronActivation = neurons[connections[i].targetNeuronIdx].activation;
+			if(connections[i].weight >= 0)
+			{
+				double delta = (connections[i].hebbRate
+						* (maxWeight - connections[i].weight)
+						* incomingNeuronActivation
+						* outgoingNeuronActivation)
+						+ connections[i].hebbPreRate * maxWeight
+						* incomingNeuronActivation
+						* outgoingNeuronActivation;
+				connections.weight = -(connections[i].weight + delta);
+			}
+			if(connections[i].weigth < 0)
+			{
+				double delta = connections[i].hebbPreRate
+						* (maxWeight - connections[i].weight)
+						* incomingNeuronActivation
+						* (1.0 - outgoingNeuronActivation)
+						- connections[i].hebbRate * maxWeight
+						* incomingNeuronActivation
+						* outgoingNeuronActivation;
+				connections.weight = -(connections[i].weight + delta);
+			}
+			double minw = -params.MaxWeight,
+			double maxw = params.MaxWeight,
+			if(connections[i].weigth < min)
+			{
+				connections[i].weight = minw;
+			}
+			else if(connections[i].weight > max)
+			{
+				connections[i].weight = maxw;
+			}
+		}
+	}
+
+	int connectionExists(int to,int from)
+	{
+		for(uint i = 0; i < connections.length;i++)
+		{
+			if((connections[i].sourceNeuronIdx == from)
+					&& (connections[i].targetNeuronIdx == to))
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	void rtrlUpdateGradients()
+	{
+		// for every neuron
+		for (uint k = numInputs; k < neurons.length; k++)
+		{
+			// for all possible connections
+			for (uint i = numInputs; i < neurons.length; i++)
+			{
+				// to
+				for (uint j = 0; j < neurons.length; j++) // from
+				{
+					int idx = connectionExists(i, j);
+					if (idx != -1)
+					{
+						//double derivative = unsigned_sigmoid_derivative( neurons[k].activation );
+						double derivative = 0;
+						if (neurons[k].activation_function_type
+								== ActivationFunction.unsignedSigmoid)
+						{
+							derivative = unsigned_sigmoid_derivative(
+									neurons[k].activation);
+						}
+						else if (neurons[k].activation_function_type
+								== ActivationFunction.tanh)
+						{
+							derivative = tanhDerivative(
+									neurons[k].activation);
+						}
+
+						double sum = 0;
+						// calculate the other sum
+						for (uint l = 0; l < neurons.length; l++)
+						{
+							int lIdx = connectionExists(k, l);
+							if (lIdx != -1)
+							{
+								sum += connections[lIdx].weight
+										* neurons[l].sensitivityMatrix[i][j];
+							}
+						}
+
+						if (i == k)
+						{
+							sum += neurons[j].activation;
+						}
+						neurons[k].sensitivityMatrix[i][j] = derivative
+								* sum;
+					}
+					else
+					{
+						neurons[k].sensitivityMatrix[i][j] = 0;
+					}
+				}
+			}
+		}
+    }
+	void rtrlUpdateError(double target)
+	{
+		totalError = (target - output()[0]);
+		// Adjust each weight
+		for(uint i = 0; i < neurons.length;i++)//to
+		{
+			for(uint j = 0; < neurons.length; j++)
+			{
+				int idx = connectionExists(i,j);
+				if(idx != -1)
+				{
+					double delta = totalError
+						* neurons[numInputs].sensitivityMatrix[i][j];
+					totalWeightChange[idx] += delta * 0.0001;
+				}
+			}
+		}
+	}
+
+	void rtrlUpdateWeights()
+	{
+		for(uint i = 0; i < connections.length; i++)
+		{
+			connections[i].weight += totalWeightChange[i];
+			totalWeightChange[i] = 0;
+		}
+		totalError = 0;
+	}
+	
+
+	////////////////////////
+	// Accessor functions //
+	////////////////////////
 	void addNeuron(Neuron n)
 	{
 		neurons ~= n;
